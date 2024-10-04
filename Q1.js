478edb207a097f11cfd0e1b563f20d8443a31cbd
Qamrul-Hassan <sajal700@gmail.com>
2024-10-04 20:08:20 +0600

    massage

---
+++ Q1.js
@@ -0,0 +1,9 @@
+//1. Write a program that determines wether a given number is possitive or negative
+
+let number =8;
+    
+if (number > 0) {
+    console.log("The number is positive");
+} else {
+    console.log (" The number is not Possitve. ");
+}
